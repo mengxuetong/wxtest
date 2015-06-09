e9ad59f54c5838db740afbd6f85ec5d813ab24e6
mengxuetong <mengxt@foxmail.com>
2015-06-09 14:25:56 +0800

    this project

---
+++ nodejs/apps-info.js
@@ -0,0 +1,17 @@
+/**
+ * 微信认证后的 app 信息
+ * 如果有需要可以将这些信息放到数据库中读取方便以更增加
+ * 注：测试时请替换您真实的appid和secret
+ */
+module.exports = function(){
+	return [
+			{
+				appid: 'wxa6a3ea9ccccddee4',
+				secret: '696f09d60e3b7e92b8ea6955f481b4c0'
+			}
+			,{
+				appid: 'e0302a0f90d9d154de9c5cf13547fcac',
+				secret: 'e0302a0f90d9d154de9c5cf13547fcac'
+			}
+		];
+};
